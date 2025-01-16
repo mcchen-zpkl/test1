932d75dc9dd8b9c41e002bc8ca8d0aab3d0b37df
mcchen-zpkl <mcchentp6a4fup6@gmail.com>
2025-01-16 11:08:31 +0800

    add 01.py & README.md
    com1
    
    com2
    
    com1
    
    com2

---
+++ 01.py
@@ -0,0 +1,11 @@
+import torch
+
+torch_version = torch.__version__
+cuda_version_pt = torch.version.cuda
+cudnn_version_pt = torch.backends.cudnn.version()
+
+print(f'PyTorch_version', torch_version)
+print(f'CUDA_Version', cuda_version_pt)
+print(f'CUDA_Available', torch.cuda.is_available())
+print(f'cuDNN_Version', cudnn_version_pt)
+print(f'GPU_Name', torch.cuda.get_device_name(0))
